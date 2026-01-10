709b76dc0a122edc57c46ecc7d53779f8cac1953
kbwo <kbwo.owbk@gmail.com>
2026-01-10 15:23:18 +0900

    refactor: fix lint error

--- src/components/ConfigureStatusHooks.tsx
+++ src/components/ConfigureStatusHooks.tsx
@@ -170,7 +170,7 @@ const ConfigureStatusHooks: React.FC<ConfigureStatusHooksProps> = ({
 				</Box>
 				<Box>
 					<Text dimColor>
-						CCMANAGER_WORKTREE_PATH, CCMANAGER_WORKTREE_BRANCH, CCMANAGER_SESSION_ID
+						{`CCMANAGER_WORKTREE_PATH, CCMANAGER_WORKTREE_BRANCH, CCMANAGER_SESSION_ID`}
 					</Text>
 				</Box>
 
